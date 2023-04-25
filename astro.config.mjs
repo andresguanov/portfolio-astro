7a2d1bd51ae0a8c7cf72bf0fa0820707f79bffdd
andresguanov <andresguanov@gmail.com>
2023-04-25 00:15:32 -0500

    [Add] MDX config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import {defineConfig} from 'astro/config'
-
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
+// https://astro.build/config
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), preact()]
+	integrations: [tailwind(), preact(), mdx()]
 })
